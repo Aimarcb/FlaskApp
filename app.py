23b6f69566222e851e2041f5f1af9c654319fd4d
RoachLok <jtabordajunior@hotmail.com>
2022-05-16 19:43:46 +0200

    Add api endpoint to query user info. Returns json.
    Co-authored-by: Aimarcb <Aimarcb@users.noreply.github.com>

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template, request, redirect, session
+from flask import Flask, jsonify, render_template
+ 
+import json
  
 app = Flask(__name__)
 
@@ -8,7 +10,6 @@ def index():
 
 @app.route('/text')
 def text():
-    f = open("data/agenda.csv", 'r')
     agenda = []
 
     filepath = "data/agenda.csv"
@@ -17,12 +18,40 @@ def text():
         while line:
             line = fp.readline()
             contact = line.split (',', 2)
-            
             agenda.append(line.split(',', 2))
-    #agenda = re.sub(r'(<)', '', agenda)
+
     return render_template("text.html", test=agenda)
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
 
-    
+@app.route("/api/get_user/<user>")
+def getUser(user):
+    agenda = []
+
+    """
+    Javier, 123
+    Aimar, 345
+    Alvaro, 543
+    """
+
+    filepath = "data/agenda.csv"
+    with open(filepath, 'r') as csv:
+        line = csv.readline()               # "Aimar, 345"
+        while line:
+            #contact = line.split (',', 2)
+            line = csv.readline()
+            # Guardamos en la agenda la línea dividiendo por ','
+            contacto = line.split(',',2)              #["Aimar", 345]    # agenda = [ ["Javier", 123] ]
+
+            agenda.append(line.split(',',2))         # agenda = [ ["Javier", 123], ["Aimar", 345], ["Alvaro", 543] ]
+
+
+    for contacto in agenda:
+        if user == contacto[0]:
+            return jsonify(name=contacto[0], number=contacto[1])
+
+    return jsonify(user=user, error="User not found."), 404
+
+
+
+if __name__ == '__main__':
+    app.run(port=5002, debug=True, host='0.0.0.0')
